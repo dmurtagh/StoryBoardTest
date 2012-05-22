5540c19d26b172288719960cb3884cb7b3d243c3
Damien Murtagh <damien.murtagh@gmail.com>
2012-05-21 22:13:23 -0700

    Initial Commit

---
+++ StoryboardTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryboardTest
+//
+//  Created by Damien Murtagh on 5/21/12.
+//  Copyright (c) 2012 Playfirst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoryboardTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoryboardTest
+//
+//  Created by Damien Murtagh on 5/21/12.
+//  Copyright (c) 2012 Playfirst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
